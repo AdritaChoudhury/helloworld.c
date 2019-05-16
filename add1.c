80ce87bfe5509759436c7858a8b09689ed6f03d3
AdritaChoudhury <50648833+AdritaChoudhury@users.noreply.github.com>
2019-05-16 09:18:51 +0530

    Create add1.c

---
+++ add1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+        int a,b;
+        printf("Enter the number:");
+        scanf("%d",&a);
+        b=a+1;
+        printf("The result after adding 1 is %d",b);
+}
