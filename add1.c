1a36f58cf14966ff6cc9a898d751ac59747869af
AdritaChoudhury <50648833+AdritaChoudhury@users.noreply.github.com>
2019-05-16 09:16:27 +0530

    Delete add1.c

--- add1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-        int a,b;
-        printf("Enter the number:");
-        scanf("%d",&a);
-        b=a+1;
-        printf("The result after adding 1 is %d",b);
-}
